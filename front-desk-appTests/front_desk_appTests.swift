f6e53f8d0bff78698ddfccf3a1c9d690f9f73a48
Animesh Manglik <animeshmanglik@SMOLTM306-2.local>
2015-06-16 22:35:21 -0700

    Initial Commit

---
+++ front-desk-appTests/front_desk_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  front_desk_appTests.swift
+//  front-desk-appTests
+//
+//  Created by Animesh Manglik on 6/16/15.
+//  Copyright (c) 2015 Animesh Manglik. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class front_desk_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
